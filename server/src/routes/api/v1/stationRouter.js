141282b8723fb9f8812bf995f8e6d5407116e9bc
julia <juliaconnor2000@gmail.com>
2023-05-03 13:47:05 -0400

    added serializer for stations

--- server/src/routes/api/v1/stationRouter.js
+++ server/src/routes/api/v1/stationRouter.js
@@ -1,12 +1,18 @@
 import express from "express"
 import { Station } from "../../../models/index.js"
+import StationSerializer from "../../../serializers/StationSerializer.js"
 
 const stationRouter = new express.Router()
 
 stationRouter.get("/", async (req, res) => {
     try {
         const stations = await Station.query()
-        return res.status(200).json({ stations: stations })
+        const serializedStations = await Promise.all(
+            stations.map(async (station) => {
+              return await StationSerializer.getSummary(station)
+            })
+          )
+        return res.status(200).json({ stations: serializedStations })
     } catch (err) {
         return res.status(500).json({ errors: err })
     }
@@ -16,7 +22,8 @@ stationRouter.get("/:id", async (req, res) => {
     const { id } = req.params
     try {
         const showStation = await Station.query().findById(id)
-        return res.status(200).json({ station: showStation })
+        const serializedStation = await StationSerializer.getSummary(showStation)
+        return res.status(200).json({ station: serializedStation })
     } catch (err) {
         return res.status(500).json({ errors: err })
     }
