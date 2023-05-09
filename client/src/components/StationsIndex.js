ab8abc18207c5e55db8650b1afbd65cbbd4fe0db
Cooper Dutfield <cooperdutfield@icloud.com>
2023-05-09 14:09:29 -0400

    styling for index page

--- client/src/components/StationsIndex.js
+++ client/src/components/StationsIndex.js
@@ -26,18 +26,28 @@ const StationIndex = props => {
 
     const stationIndexItems = stations.map(stationItem => {
         return (
-            <li key={stationItem.id}>
+            <div className="tile-border">
+            <h4 className="font-apply" key={stationItem.id}>
                 <Link to={`/stations/${stationItem.id}`}>
                     {stationItem.name}
+                    <br></br>
                 </Link>
-            </li>
+                    {stationItem.line}
+                    <br></br>
+                    {stationItem.location}
+                    <div className="img-format">
+                   <img className="img-format" src={stationItem.imgUrl} alt="station picture"/>
+                   </div>
+            </h4>
+            </div>
         )
     })
 
     return (
         <div>
-            <h1>Boston T Stations</h1>
-            <ul>{stationIndexItems}</ul>
+            <h1 className="font-apply index-top-text">Boston T Stations</h1>
+            <br></br>
+            <div className="hi"> {stationIndexItems} </div>
         </div>
     )
 }
