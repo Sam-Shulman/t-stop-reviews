c5a052040ccd8297d4e80b4788310de336aa23af
Sam Shulman <sshulman39@gmail.com>
2023-05-11 14:15:29 -0400

    one thing

--- server/src/db/Seeder.js
+++ server/src/db/Seeder.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-console */
 import { connection } from "../boot.js"
 import StationSeeder from "./seeders/StationSeeder.js"
-import UsersSeeder from "./seeders/UsersSeeder.js"
 class Seeder {
   static async seed() {
     console.log("seeding stations")
-    await UsersSeeder.seed()
     await StationSeeder.seed()
     console.log("Done!")
     await connection.destroy()
